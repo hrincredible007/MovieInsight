6f2ae31d0ba6bfe684827e461b6941cbebdc2d13
hrincredible007 <hritikbhardwaj2002@gmail.com>
2023-09-15 13:48:49 +0530

    What's Popular Section Completed

--- src/components/carousel/Carousel.js
+++ src/components/carousel/Carousel.js
@@ -14,7 +14,7 @@ import Genres from "../genres/Genres";
 
 import "./style.scss";
 
-const Carousel = ({ data, loading }) => {
+const Carousel = ({ data, loading, endpoint }) => {
   const carouselContainer = useRef();
   const { url } = useSelector((state) => state.home);
   const navigate = useNavigate();
@@ -68,7 +68,7 @@ const Carousel = ({ data, loading }) => {
                 <div
                   key={item.id}
                   className="carouselItem"
-                  onClick={() => navigate(`/${item.media_type}/${item.id}`)}
+                  onClick={() => navigate(`/${item.media_type || endpoint}/${item.id}`)}
                 >
                   <div className="posterBlock">
                     <Img src={posterUrl} />
--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -2,13 +2,17 @@ import React from "react";
 import "./style.scss";
 import Trending from "./trending/Trending";
 import HeroBanner from "./heroBanner/HeroBanner";
+import Popular from "./popular/Popular";
+import TopRated from "./topRated/TopRated";
 
 const Home = () => {
   return (
     <>
       <div className="homePage">
-        <HeroBanner></HeroBanner>
-        <Trending></Trending>
+        <HeroBanner/>
+        <Trending/>
+        <Popular/>
+        <TopRated/>
       </div>
     </>
   );
